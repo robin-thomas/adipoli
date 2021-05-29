497edb4334f53e463b7e3f988a48afd8bbd6eb23
Robin Thomas <robinthomas2591@gmail.com>
2021-05-29 12:23:06 +0800

    create home components

---
+++ components/container.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './container.module.css';
+
+const Container = ({ title, children }) => (
+  <div className={styles.container}>
+    <Head>
+      <title>{title}</title>
+      <link rel="icon" href="/images/favicon.ico" />
+    </Head>
+
+    <main>
+      {children}
+    </main>
+
+    <footer>
+      <a
+        href="https://www.rapyd.net/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <Image src="/images/rapyd.svg" alt="Rapyd Logo" height="19" width="75" />
+      </a>
+    </footer>
+  </div>
+);
+
+export default Container;
